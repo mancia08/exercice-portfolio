d4422d2f35aeb0841c552493bccd0265e1fa0f6f
Teiko <teikomancini1993@gmail.com>
2020-11-27 17:08:21 +0100

    button done

---
+++ src/components/organisms/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from "./../atoms/Button"
+
+function Header(props) {
+    return (
+        <div>
+            <Button/>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/styles/theme.js
@@ -0,0 +1,27 @@
+export const style = {
+  color: {
+    primary: `#0699A6`,
+    secondary: `#9E8C6C`,
+    text: `#000000`,
+    btnText: `#FFFFFF`,
+  },
+  font: {
+    family: {
+      main: {
+        url:
+          "https://fonts.googleapis.com/css?family=PT+Sans+Narrow&display=swap",
+        family: "PT Sans Narrow",
+      },
+    },
+    weight: { bold: "bold" },
+    size: {
+      mobile: { xs: 14, s: 18, m: 26, l: 32, xl: 40, xxl: 56 },
+    },
+  },
+  sizes: {
+    btn:{
+      width: 225,
+      height: 60
+    }
+  }
+};
